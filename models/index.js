983464907c97d1837ac496eacd6141f6e1b9a5d5
thaerbraizat <thaerbraizat13@gmail.com>
2021-08-18 13:36:15 +0300

    dep1

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('dotenv').config();
-const POSTGRES_URI = 'postgres://localhost:5432/thaerbraizat';
+const POSTGRES_URI =process.env.DATABASE_URL ;
 const { Sequelize, DataTypes } = require('sequelize');
 
 
